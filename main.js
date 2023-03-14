19347129d87947b9ca4951fdf08c231bbd53557e
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-03-14 20:48:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,20 @@ function setup(){
     poseNet=ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+function gotPoses(results){
+ if(results.length >0){
+    console.log(results);
+    noseX = results[0].pose.nose.x-100;
+    noseY = results[0].pose.nose.y-75;
+ }
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 200, 150);
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
